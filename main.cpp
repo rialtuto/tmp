92926eb5c531944ad3cd7dad29586b5c62af68ba
rialtuto <tutorial@onet.pl>
2018-07-06 04:04:51 +0200

    'feature_one'

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    const char* result = add("Ala ", "ma kota.");
+    const char* result = add("Ala ", "ma psa i kota.");
     cout << result << endl;
     delete []result;
     return 0;
